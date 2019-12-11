1a00bed0a4665ea64f6bdbf45c5716bc74b4cfdc
allison_johnson <allison.cuttler@gmail.com>
2019-12-11 13:11:12 -0800

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,17 @@
+class Dog
+  def name=(a_name)
+    @name = a_name
+  end #name=
+
+  def breed=(a_breed)
+    @breed = a_breed
+  end #breed=
+
+  def name
+    @name
+  end #name
+
+  def breed
+    @breed
+  end #breed
+end #class
---
+++ lib/person.rb
@@ -0,0 +1,17 @@
+class Person
+  def name=(a_name)
+    @name = a_name
+  end #name=
+
+  def job=(a_job)
+    @job = a_job
+  end #job=
+
+  def name
+    @name
+  end
+
+  def job
+    @job
+  end 
+end #class
